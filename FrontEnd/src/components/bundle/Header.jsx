2aa979e8bd13f8dadc35e275e2f998d225fd0f5b
Graziano Corineo <grazianocorineo01@gmail.com>
2023-08-27 01:01:43 +0200

    Update Navbar - HamburgerMenu -> Home

---
+++ FrontEnd/src/components/bundle/Header.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./Navbar";
+
+const Header = () => {
+
+
+
+  const imgHeader = {
+    backgroundImage: "url('/src/assets/pexels-photo-3296392.jpeg')",
+  };
+
+
+
+
+  return (
+    <>
+      <Navbar />
+      <header>
+        <div className="h-[40rem] w-full bg-cover bg-center" style={imgHeader}>
+          <div className="flex flex-col items-center justify-center h-full ">
+            {" "}
+            <h1 className="text-fourth text-3xl tracking-wide font-semibold">FLOW</h1>
+            <h1 className="text-fourth text-3xl tracking-wide font-semibold py-5">Sushi Experience</h1>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
---
+++ FrontEnd/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "300px",
+
+      s: "548px",
+
+      md: "768px",
+
+      mdd: "857px",
+
+      x: "1025",
+
+      xl: "1280px",
+
+      "2xl": "1451px",
+    },
+    extend: {
+      colors: {
+        primary: "#72383d", // Bordeaux
+        secondary: "#AC9C8D", // Marron
+        tertiary: " #322D29", // black footer
+        fourth: "  #ffff", // white
+        invert: " #72383d", // red
+      },
+    },
+  },
+  plugins: [],
+};
