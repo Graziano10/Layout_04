049412cd0f39f00b2e0e20aaf1a7cbe94515ed0c
Graziano Corineo <grazianocorineo01@gmail.com>
2023-08-30 19:01:13 +0200

    Update Footer -> Home

--- FrontEnd/src/components/bundle/Main04.jsx
+++ FrontEnd/src/components/bundle/Main04.jsx
@@ -4,20 +4,20 @@ const Main04 = () => {
       <div className="bg-primary">
         <div className="flex justify-center items-center h-full">
           {" "}
-          <h1 className="text-fourth font-semibold text-center text-3xl md:text-5xl x:text-2xl">
+          <h1 className="text-fourth font-semibold text-center text-3xl p-5 md:text-5xl x:text-2xl pt-5">
             Opening hours
           </h1>
         </div>
-        <div className="h-full flex justify-center items-center gap-10 p-5">
-            <p>Monday</p>
-            <p>18:00-00:00</p>
+        <div className="text-fourth text-normal flex-col  h-full flex justify-center items-center gap-2 p-3 tracking-wide x:flex-row x:gap-10 x:p-5">
+          <p>Monday</p>
+          <p>18:00-00:00</p>
         </div>
-        <div className="h-full flex justify-center items-center gap-10 p-5">
-            <p>Martedi - Sunday</p>
-            <div className="flex flex-col">
-                <p>12:00-16:00</p>
-                <p>18:00-00:00</p>
-            </div>
+        <div className="text-fourth text-normal flex-col h-full flex justify-center items-center gap-2 p-3 tracking-wide x:flex-row x:gap-10 x:p-5 x:pb-9 ">
+          <p>Tuesday - Sunday</p>
+          <div className="flex flex-col">
+            <p>12:00-16:00</p>
+            <p>18:00-00:00</p>
+          </div>
         </div>
       </div>
     </>
